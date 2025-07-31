83ecb3d3b422423ff9f37d7921c8cf0bb3528014
Edgar <120991424+junerks@users.noreply.github.com>
2025-07-30 20:39:15 -0400

    Most of home page finished

--- src/app/services/pages/home/home-service.ts
+++ src/app/services/pages/home/home-service.ts
@@ -7,6 +7,7 @@ import { ApiService } from '../../api-service';
 export class HomeService extends ApiService {
   constructor() {
     super();
-    this.url = '/api/home?';
+    this.url =
+      '/api/home?populate[0]=banner&populate[1]=enterpriseServices&populate[2]=enterpriseServices.serviceButtons&populate[3]=enterpriseProducts&populate[4]=enterpriseProducts.productCards&populate[5]=enterpriseProducts.productCards.image&populate[6]=enterpriseProducts.productCards.button&populate[7]=achievements&populate[8]=achievements.achievementItems';
   }
 }
